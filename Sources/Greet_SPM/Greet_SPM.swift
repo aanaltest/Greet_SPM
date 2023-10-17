e0cd7d73c262b5b038c6de92d13aa1eca7539c91
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-17 10:15:47 +0530

    v1-good evening function added

--- Sources/Greet_SPM/Greet_SPM.swift
+++ Sources/Greet_SPM/Greet_SPM.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+public class Greet_SPM {
+    
+    public init() {}
+    
+    public func greetUserGoodEvening(name: String) {
+        print("Good Evening \(name) !!!!!")
+    }
+}
+
